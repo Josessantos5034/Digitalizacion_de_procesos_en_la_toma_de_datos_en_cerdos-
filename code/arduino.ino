441ccde218b32292369b74fd663e73f6f47582c6
Josessantos5034 <josesantos503410@gmail.com>
2021-04-05 10:46:41 -0600

    Alineo de las lineas de codigo

--- code/arduino.ino
+++ code/arduino.ino
@@ -1,15 +1,15 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 #include <Q2HX711.h>
-LiquidCrystal_I2C lcd(0x3f,20,4);
+LiquidCrystal_I2C lcd(0x3f, 20, 4);
 //pines//
-const byte hx711_data_pin =3;
+const byte hx711_data_pin = 3;
 const byte hx711_clock_pin = 2;
 int tara_button = 8;
 int mode_button = 11;
 Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
 // variables //
-float y1= 2831.0; //la mediada que tenga el objeto//
+float y1 = 2831.0; //la mediada que tenga el objeto//
 long x1 = 0L;
 long x0 = 0L;
 float avg_size = 10.0;
@@ -19,10 +19,10 @@ bool mode_pushed = false;
 int mode = 0;
 float oz_conversion = 0.035274;
 void setup() {
- Serial.begin(9600);
- PCICR |= (1 << PCIE0);
- PCMSK0 |= (1 << PCINT0);
- PCMSK0 |= (1 << PCINT3);
+  Serial.begin(9600);
+  PCICR |= (1 << PCIE0);
+  PCMSK0 |= (1 << PCINT0);
+  PCMSK0 |= (1 << PCINT3);
   pinMode(tara_button, INPUT_PULLUP);
   pinMode(mode_button, INPUT_PULLUP);
 
@@ -32,109 +32,109 @@ void setup() {
   delay (1000);
 
 
-  for (int ii= 0; ii<int(avg_size); ii++){
+  for (int ii = 0; ii < int(avg_size); ii++) {
     delay(10);
-    x0+=hx711.read();
+    x0 += hx711.read();
   }
-x0/=long(avg_size);
+  x0 /= long(avg_size);
   Serial.println("Add Calibrated Mass");
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print(" Add Calibrated ");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("      Mass      ");
- //calibracion//
- int ii= 1;
- while(true){
-  if (hx711.read()<x0+10000)
-   {
+  //calibracion//
+  int ii = 1;
+  while (true) {
+    if (hx711.read() < x0 + 10000)
+    {
 
-   }
-   else
-   {
-    ii++;
+    }
+    else
+    {
+      ii++;
       delay(2000);
-      for (int jj=0;jj<int(avg_size);jj++){
-        x1+=hx711.read();
-   }
-   x1/=long(avg_size);
-    break;
- }
-}
-Serial.println("Calibration Complete");
+      for (int jj = 0; jj < int(avg_size); jj++) {
+        x1 += hx711.read();
+      }
+      x1 /= long(avg_size);
+      break;
+    }
+  }
+  Serial.println("Calibration Complete");
   lcd.clear();
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("  Calibration   ");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("    Complete    ");
 }
 
 void loop() {
-//lectura promedio//
-long reading = 0;
-  for (int jj=0;jj<int(avg_size);jj++)
+  //lectura promedio//
+  long reading = 0;
+  for (int jj = 0; jj < int(avg_size); jj++)
   {
-    reading+=hx711.read();
+    reading += hx711.read();
   }
-  reading/=long(avg_size);
+  reading /= long(avg_size);
 
-// calculo para calibracion //
+  // calculo para calibracion //
 
-float ratio_1 = (float) (reading-x0);
-  float ratio_2 = (float) (x1-x0);
-  float ratio = ratio_1/ratio_2;
-  float mass = y1*ratio;
-  if(tara_pushed)
+  float ratio_1 = (float) (reading - x0);
+  float ratio_2 = (float) (x1 - x0);
+  float ratio = ratio_1 / ratio_2;
+  float mass = y1 * ratio;
+  if (tara_pushed)
   {
     tara = mass;
     tara_pushed = false;
     Serial.print("TARA");
     Serial.print(".");
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("      .         ");
     delay(300);
     Serial.print(".");
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("      ..        ");
     delay(300);
     Serial.println(".");
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("      ...       ");
     delay(300);
   }
- if(mode_pushed)
+  if (mode_pushed)
   {
     mode = mode + 1;
     mode_pushed = false;
-    if(mode > 2){
+    if (mode > 2) {
       mode = 0;
     }
   }
-  if(mode == 0)
+  if (mode == 0)
   {
     Serial.print(mass - tara);
     Serial.println(" g");
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(mass - tara);
     lcd.print(" g");
   }
-  else if(mode == 1)
+  else if (mode == 1)
   {
     Serial.print(mass - tara);
     Serial.println(" ml");
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print(mass - tara);
     lcd.print(" ml");
   }
@@ -143,9 +143,9 @@ float ratio_1 = (float) (reading-x0);
     Serial.print((mass - tara)*oz_conversion);
     Serial.println(" oz");
     lcd.clear();
-    lcd.setCursor(0,0);
+    lcd.setCursor(0, 0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print((mass - tara)*oz_conversion);
     lcd.print(" oz");
   }
