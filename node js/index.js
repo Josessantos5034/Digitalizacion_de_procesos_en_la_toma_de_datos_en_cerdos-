889559b1c93be10fb7e8b0878ea52901d8edbeb0
Josessantos5034 <josesantos503410@gmail.com>
2021-04-15 17:22:02 -0600

    Se borro la comunicacion serial para poder usar hacer FULL IOT

--- node js/index.js
+++ node js/index.js
@@ -4,11 +4,7 @@ const bot = new TelegramBot(token, {
   polling: true
 });
 var Idmichat = 923905754;
-var SerialPort = require ('serialport');
-var MiPuerto = new SerialPort('COM3', {
-baudRate: 9600,
-autoOpen : true
-});
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El ID del chat" + chatId);
@@ -23,13 +19,4 @@ bot.on('message', (msg) => {
       bot.sendMessage(chatId, 'OFF');
         MiPuerto.write("L");
     }
-});
-
-MiPuerto.setEncoding('utf8');
-MiPuerto.on('data', function(data) {
-  console.log("Lo que entro es" + data);
-  if (data[0] == 'H'){
-    console.log("Boton Precionado");
-    bot.sendMessage(Idmichat,"Se preciono el boton");
-  }
 });
