42cd961b02937bdf099e4d94daae240721f62014
Josessantos5034 <josesantos503410@gmail.com>
2021-04-17 20:04:37 -0600

    correcion de mqtt connect y de los parametro de publicacion

--- node js/index.js
+++ node js/index.js
@@ -1,22 +1,26 @@
 const TelegramBot = require('node-telegram-bot-api');
+var mqtt = require('mqtt');
 const token = '1763212757:AAFCdM0oLLv2VUCkgwWGw6A2g5jJbNnJKy8';
 const bot = new TelegramBot(token, {
   polling: true
+
 });
 var Idmichat = 923905754;
+
+var client  = mqtt.connect('mqtt://cerdos-prueba:pEcontDiDMEvV6rT@cerdos-prueba.cloud.shiftr.io');
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El ID del chat" + chatId);
-  var Mensaje = msg.text;
-  if (Mensaje == "encender"){
+  var Mensaje = msg.text.toString().toLocaleLowerCase();
+  if (Mensaje.indexOf("encender") >=0){
     console.log("encendiendo el led");
     bot.sendMessage(chatId, 'ON');
-    MiPuerto.write("H");
+    client.publish('temp','1');
     }
-    else if (Mensaje == "apagar"){
+    else if(Mensaje.indexOf("apagar") >=0){
       console.log("apagando el led");
       bot.sendMessage(chatId, 'OFF');
-        MiPuerto.write("L");
+      client.publish('temp','0');
     }
 });
