02ac342eea1bcdd83af467fae6886c98bb664cd2
Josessantos5034 <josesantos503410@gmail.com>
2021-05-02 20:17:02 -0600

    codigo bascula

--- codigos/BASCULA/BASCULA.ino
+++ codigos/BASCULA/BASCULA.ino
@@ -1,156 +1,176 @@
-#include <LiquidCrystal_I2C.h>
-#include <Wire.h>
+
 #include <Q2HX711.h>
-LiquidCrystal_I2C lcd(0x3f, 20, 4);
-//pines//
-const byte hx711_data_pin = 3;
-const byte hx711_clock_pin = 2;
-int tara_button = 8;
-int mode_button = 11;
-Q2HX711 hx711(hx711_data_pin, hx711_clock_pin);
-// variables //
-float y1 = 2831.0; //la mediada que tenga el objeto//
+//LCD config
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27,20,4);
+#include <LiquidCrystal_I2C.h>
+
+
+//Pins
+const byte hx711_data_pin = 3;    //Data pin from HX711
+const byte hx711_clock_pin = 2;   //Clock pin from HX711
+int tara_button = 8;              //Tara button
+int mode_button = 11;             //Mode change button
+Q2HX711 hx711(hx711_data_pin, hx711_clock_pin); // prep hx711
+
+//Variables
+/////////Change here with your calibrated mass////////////
+float y1 = 292875; // calibrated mass to be added
+//////////////////////////////////////////////////////////
+
 long x1 = 0L;
 long x0 = 0L;
-float avg_size = 10.0;
+float avg_size = 10.0; // amount of averages for each mass measurement
 float tara = 0;
 bool tara_pushed = false;
 bool mode_pushed = false;
 int mode = 0;
 float oz_conversion = 0.035274;
+//////////////////////////////////////////////////////////
+
+
+
 void setup() {
-  Serial.begin(9600);
-  PCICR |= (1 << PCIE0);
-  PCMSK0 |= (1 << PCINT0);
-  PCMSK0 |= (1 << PCINT3);
+  Serial.begin(9600);                 // prepare serial port
+  PCICR |= (1 << PCIE0);              //enable PCMSK0 scan
+  PCMSK0 |= (1 << PCINT0);            //Set pin D8 trigger an interrupt on state change.
+  PCMSK0 |= (1 << PCINT3);            //Set pin D10 trigger an interrupt on state change.
   pinMode(tara_button, INPUT_PULLUP);
   pinMode(mode_button, INPUT_PULLUP);
 
-  //iniciar pantalla lcd//
-  lcd.init();
-  lcd.backlight();
-  delay (1000);
+  lcd.init();                         //Init the LCD
+  lcd.backlight();                    //Activate backlight
+
+  delay(1000);                        // allow load cell and hx711 to settle
 
 
-  for (int ii = 0; ii < int(avg_size); ii++) {
+  // tare procedure
+  for (int ii=0;ii<int(avg_size);ii++){
     delay(10);
-    x0 += hx711.read();
+    x0+=hx711.read();
   }
-  x0 /= long(avg_size);
+  x0/=long(avg_size);
   Serial.println("Add Calibrated Mass");
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);
   lcd.print(" Add Calibrated ");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0,1);
   lcd.print("      Mass      ");
-  //calibracion//
+  // calibration procedure (mass should be added equal to y1)
   int ii = 1;
-  while (true) {
-    if (hx711.read() < x0 + 10000)
+  while(true){
+    if (hx711.read()<x0+10000)
     {
-
+      //do nothing...
     }
     else
     {
       ii++;
       delay(2000);
-      for (int jj = 0; jj < int(avg_size); jj++) {
-        x1 += hx711.read();
+      for (int jj=0;jj<int(avg_size);jj++){
+        x1+=hx711.read();
       }
-      x1 /= long(avg_size);
+      x1/=long(avg_size);
       break;
     }
   }
   Serial.println("Calibration Complete");
   lcd.clear();
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0,0);
   lcd.print("  Calibration   ");
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0,1);
   lcd.print("    Complete    ");
 }
 
 void loop() {
-  //lectura promedio//
+  // averaging reading
   long reading = 0;
-  for (int jj = 0; jj < int(avg_size); jj++)
+  for (int jj=0;jj<int(avg_size);jj++)
   {
-    reading += hx711.read();
+    reading+=hx711.read();
   }
-  reading /= long(avg_size);
+  reading/=long(avg_size);
 
-  // calculo para calibracion //
 
-  float ratio_1 = (float) (reading - x0);
-  float ratio_2 = (float) (x1 - x0);
-  float ratio = ratio_1 / ratio_2;
-  float mass = y1 * ratio;
-  if (tara_pushed)
+
+  // calculating mass based on calibration and linear fit
+  float ratio_1 = (float) (reading-x0);
+  float ratio_2 = (float) (x1-x0);
+  float ratio = ratio_1/ratio_2;
+  float mass = y1*ratio;
+
+  if(tara_pushed)
   {
     tara = mass;
     tara_pushed = false;
     Serial.print("TARA");
     Serial.print(".");
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print("      .         ");
     delay(300);
     Serial.print(".");
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print("      ..        ");
     delay(300);
     Serial.println(".");
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("      TARA      ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print("      ...       ");
     delay(300);
   }
-  if (mode_pushed)
+  if(mode_pushed)
   {
     mode = mode + 1;
     mode_pushed = false;
-    if (mode > 2) {
+    if(mode > 2)
+    {
       mode = 0;
     }
   }
-  if (mode == 0)
+
+  if(mode == 0)
   {
     Serial.print(mass - tara);
     Serial.println(" g");
     lcd.clear();
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print(mass - tara);
     lcd.print(" g");
   }
-  else if (mode == 1)
+  else if(mode == 1)
   {
     Serial.print(mass - tara);
-    Serial.println(" ml");
+    Serial.println(" kg");
     lcd.clear();
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print(mass - tara);
-    lcd.print(" ml");
+    lcd.print(" kg");
   }
   else
   {
     Serial.print((mass - tara)*oz_conversion);
-    Serial.println(" oz");
+    Serial.println(" lb");
     lcd.clear();
-    lcd.setCursor(0, 0);
+    lcd.setCursor(0,0);
     lcd.print("     SCALE!     ");
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0,1);
     lcd.print((mass - tara)*oz_conversion);
-    lcd.print(" oz");
+    lcd.print(" lb");
   }
-}
-// interruptores//
+
+}//End of void loop
+
+
+//interruption to detect buttons
 ISR(PCINT0_vect)
 {
   if (!(PINB & B00000001))
