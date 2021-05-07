d0e60d6b9d7f10461841415b6bcf7049e55a1364
Josessantos5034 <josesantos503410@gmail.com>
2021-05-06 21:50:56 -0600

    para leer la data por medio de puerto serial

--- node js/index.js
+++ node js/index.js
@@ -1,14 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api');
-var mqtt = require('mqtt');
-const token = '1763212757:AAFCdM0oLLv2VUCkgwWGw6A2g5jJbNnJKy8';
-const bot = new TelegramBot(token, {
+//var mqtt = require('mqtt');/
+var Constantes = require('./Token');
+const bot = new TelegramBot(Constantes,token, {
   polling: true
 
 });
 var Idmichat = 923905754;
 ///ES EL URL DEL TOKEN, EL SOLO SE GENERA DEPUES DE CREAR EL TOKEN/
-var client  = mqtt.connect('mqtt://cerdos-prueba:pEcontDiDMEvV6rT@cerdos-prueba.cloud.shiftr.io');
 
+//var client  = mqtt.connect('mqtt://cerdos-prueba:pEcontDiDMEvV6rT@cerdos-prueba.cloud.shiftr.io');/
+var SerialPort = require('serialport');
+var MiPuerto = new SerialPort('COM4');
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El ID del chat" + chatId);
